18f57c072a306fa6150d4ad9f39a045e9e5f87e5
rafaan <ranvari@gmail.com>
2016-04-01 12:30:57 -0400

    Update simpleLinearRegressionGradientDescent.py

--- simpleLinearRegressionGradientDescent.py
+++ simpleLinearRegressionGradientDescent.py
@@ -1,5 +1,7 @@
 import numpy as np
 from math import sqrt
+
+# Generating example data and setting initial gradient descent parameters
 x = np.array([0,1,2,3,4])
 y = np.array([1,3,7,13,21])
 data = np.array([x, y])
@@ -8,6 +10,11 @@ step_size = .05
 tolerance = .01
 
 def simpleLinearRegressionGradientDescent(data, step_size, tolerance):
+    '''
+    Simple Linear Regression Using Gradient Descent
+    
+    
+    '''
     converged = False
     intercept = 0
     slope = 0
@@ -19,16 +26,16 @@ def simpleLinearRegressionGradientDescent(data, step_size, tolerance):
         # update intercept
         derivative_intercept = sum(errors)
         intercept = intercept - (step_size*derivative_intercept)
-        #print intercept
+        # update slope
         derivative_slope = sum(errors*data[0])
         slope = slope - (step_size*derivative_slope)
-        #print slope
+        
         gradient_sum_squares = derivative_intercept**2 + derivative_slope**2
         magnitude = sqrt(gradient_sum_squares)
-        #print magnitude
-
+        
         if magnitude < tolerance:
             converged = True
     return(intercept, slope)
-   
+
+# Fitting the model using the above data
 simpleLinearRegressionGradientDescent(data, step_size, tolerance)
